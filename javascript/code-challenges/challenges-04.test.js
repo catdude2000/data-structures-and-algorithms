d09d840eed0b77a328f1c465fd9704ef71c77517
Michael Pace <michael.g.pace@gmail.com>
2022-05-09 16:19:25 -0500

    added code challenges 1 through 4

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -2,15 +2,10 @@
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
-
-Write a function named addAnimal that takes in array of animals (strings) and some callback function.
-
+Write a function named updateAnimal that takes in array of animals (strings) and some callback function.
 This function should first create a new array. Then iterate over the input array and modify each value based on the callback function provided.
-
 Push each updated animal string into the new array. Return the new array.
-
 HINT: Look at the tests to see how the callback functions are used.
-
 ------------------------------------------------------------------------------------------------ */
 
 function upper(str) {
@@ -27,9 +22,7 @@ const updateAnimal = (arr, callback) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
 Write a function called sortNames that takes an array of names and sorts them alphabetically. Capital letters should come before lowercase letters.
-
 For example: 'Cat' would come before 'apple'
 ------------------------------------------------------------------------------------------------ */
 
@@ -39,9 +32,7 @@ const sortNames = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
 Write a function called sortNumbers that takes an array of numbers and sorts them from smallest to largest.
-
 HINT: Beware... JS default is "Lexical" ordering.
 ------------------------------------------------------------------------------------------------ */
 
@@ -51,9 +42,7 @@ const sortNumbers = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 Write a function named sortBackwards that takes in an array of numbers and returns the same array, with the numbers sorted, largest to smallest.
-
 HINT: Do it with a custom sort callback, not with using `.reverse()`. ;)
 ------------------------------------------------------------------------------------------------ */
 
@@ -63,11 +52,8 @@ const sortBackwards = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
-
 Write a function named alphabetize that takes in an array of strings and returns the same array with the strings sorted alphabetically.
-
 In this alphabetization, capital letters come before lower case letters.
-
 For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 ------------------------------------------------------------------------------------------------ */
 
@@ -77,9 +63,7 @@ const alphabetize = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
-
 Write a function named sortByPrice that takes in an array of objects, each of which has a 'price' property, and sorts those objects by price, lowest to highest, returning the same array.
-
 Here is an example of the input:
 [
   {name: 'Sweatshirt', price: 45},
@@ -87,20 +71,14 @@ Here is an example of the input:
   {name: 'Tote bag', price: 15}
 ];
 ------------------------------------------------------------------------------------------------ */
-objs.sort((a, b) = > {
-  if (a.price < b.price) {return -1; }
-  if (a.price > b.price) {return +1; }
-  else { return 0; }
-})
+
 const sortByPrice = (arr) => {
   // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
-
 Write a function named alphabetizeBetter that takes in an array of strings and returns the same array, with the strings sorted alphabetically. Capitalization should not change the sort order of two strings.
-
 For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, and so is ['alphabet', 'Alphabet', 'carrot', 'Zebra'].
 ------------------------------------------------------------------------------------------------ */
 
@@ -110,7 +88,6 @@ const alphabetizeBetter = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
-
 Write a function named sortByLength that takes in an array of strings and returns the same array, with the strings sorted by their length, lowest to highest.
 ------------------------------------------------------------------------------------------------ */
 
@@ -120,9 +97,7 @@ const sortByLength = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
-
 Write a function named sortNumbersByLength that takes in an array of numbers and sorts those numbers by their length.
-
 For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
@@ -132,7 +107,6 @@ const sortNumbersByLength = (arr) => {
 
 /*-----------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
-
 Write a function named sortPeople that takes in an array of Person objects, each of which has firstName, lastName, and age properties, and sorts those people by their last names. Do not worry about capitalization or first names.
 ------------------------------------------------------------------------------------------------ */
 
@@ -154,11 +128,8 @@ const sortPeople = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 11 - Stretch Goal
-
 Write a function named sortPeopleBetter that takes in an array of Person objects, each of which has firstName, lastName, and age properties, and sorts those people by their last names.
-
 If two people share the same last name, alphabetize on their first name.
-
 If two people have the same full name, the younger one should come first. Do not worry about capitalization.
 ------------------------------------------------------------------------------------------------ */
 
@@ -168,9 +139,7 @@ const sortPeopleBetter = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 12 - Stretch Goal
-
 Write a function named sortMeetingsByDay that takes in an array of objects, each of which represents a meeting happening a particular day of the week, with a particular start time and end time.
-
 Sort the meetings by the day on which they happen, Monday-Friday. It does not matter which order meetings come in on a particular day. For example, if there are two meetings on Monday, it does not matter which comes first.
 ------------------------------------------------------------------------------------------------ */
 
@@ -194,12 +163,9 @@ const sortMeetingsByDay = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 13 - Stretch Goal
-
-This challenge should use the array of meetings from challenge 9, above.
-
+This challenge should use the array of meetings from challenge 12, above.
 Sort the meetings in the order that they start. If two meetings start at the same time on the same day, the shorter meeting should come first.
-
-You DO NOT need to use your solution to Challenge 9 in completing Challenge 10.
+You DO NOT need to use your solution to Challenge 12 in completing Challenge 13.
 ------------------------------------------------------------------------------------------------ */
 
 const sortSchedule = (arr) => {
@@ -208,11 +174,8 @@ const sortSchedule = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenges-03.test.js
 ------------------------------------------------------------------------------------------------ */
 
