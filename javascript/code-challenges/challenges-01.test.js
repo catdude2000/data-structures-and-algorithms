13d4910e0219a1769f8ec67f79ddbcbca2d2507b
catdude2000 <michael.g.pace@gmail.com>
2022-05-09 21:35:46 -0500

    some changes to second half of 5, still not done

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -78,10 +78,11 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  num.forEach((arr) =>{
-    
-  }
-  )
+  // let newArray = [];
+  arr.forEach((num) =>{
+    arr.push(callback(num));
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -148,7 +149,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add the number 8 to the array five times', () => {
     expect(addNumbers(8, [], 5, addValues)).toStrictEqual([8, 8, 8, 8, 8]);
     expect(addNumbers(8, [], 5, addValues).length).toStrictEqual(5);
