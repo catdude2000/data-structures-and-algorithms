4c3fcf8212966fff298acc97bb0880fac8487ebc
Michael Pace <michael.g.pace@gmail.com>
2022-05-10 17:44:27 -0500

    for a pr

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -31,7 +31,7 @@ const addOne = (arr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named addQuestion that, given an array of strings, uses map to return a new array containing each string followed by a question mark character.
+Write a function named addQuestion that, given an array of strings, uses map to return a new array containing each string followed by a question mark character. 
 ------------------------------------------------------------------------------------------------ */
 
 const addQuestion = (arr) => {
